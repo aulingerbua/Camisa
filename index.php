22d2c0ef13618cbef5fd7b1263c41fbe48b1e606
Armin Aulinger <aulingerbua@web.de>
2021-03-13 15:18:58 +0100

    searching now for the name of the page in the index file beacause the unique field of Pages has been changed to id

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // phpinfo(); CaMiSa
-session_start ();
+session_start();
 
 require_once 'plug.php';
 
@@ -8,32 +8,34 @@ require_once 'plug.php';
  * set the page to show if allowed,
  * by default show the home page
  */
-$seitset = new Pages ();
-$page = $_GET ["page"];
+$seitset = new Pages();
+$page = $_GET["page"];
 
-if ($page && ! $seitset->exist ( $page )) {
-	//header ( "LOCATION: " . HOME . "errors/fileNotFound.php");
-	header ($_SERVER["SERVER_PROTOCOL"]. " 404 Not Found" );
-	exit ();
+if ($page && ! $seitset->exist([
+    'name' => $page
+])) {
+    // header ( "LOCATION: " . HOME . "errors/fileNotFound.php");
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    exit();
 }
 
-if ($seitset->visible ( $page, $_SESSION ['level'] ) === FALSE) {
-	// header($_SERVER["SERVER_PROTOCOL"].'HTTP/1.1 403 Forbidden');
-	header ( "LOCATION: " . HOME . "login?forbidden" );
-	exit ();
+if ($seitset->visible($page, $_SESSION['level']) === FALSE) {
+    // header($_SERVER["SERVER_PROTOCOL"].'HTTP/1.1 403 Forbidden');
+    header("LOCATION: " . HOME . "login?forbidden");
+    exit();
 }
 
 include_once 'html_head.php';
 
 // load the requested page
 if ($page) {
-	if ($tmpl = $seitset->getTemplate ( $page ) ['template']) {
-		include_once PagesPath . $tmpl . '.tpl.php';
-	} else {
-		include_once PagesPath . 'default.tpl.php';
-	}
+    if ($tmpl = $seitset->getTemplate($page)['template']) {
+        include_once PagesPath . $tmpl . '.tpl.php';
+    } else {
+        include_once PagesPath . 'default.tpl.php';
+    }
 } else {
-	include_once PagesPath . 'home.tpl.php';
+    include_once PagesPath . 'home.tpl.php';
 }
 
 include_once 'html_foot.php';
